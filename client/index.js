d27c13a28bed4af5847f2d082f2e03dbe71b366a
Chris Oyler <christopher.oyler@gmail.com>
2015-04-16 19:11:19 -0700

    #7 add array of objects | delete array of usernames

--- client/index.js
+++ client/index.js
@@ -6,17 +6,25 @@ function init() {
   generateTiles();
 }
 
-var gitIds = ['samerbuna', 'IAmEddieDean', 'dhh', 'ojangali'];
-
+//var gitIds = ['samerbuna', 'IAmEddieDean', 'dhh', 'ojangali'];
+//var eventCount = [];
+var profiles = [
+  {un:'samerbuna', dailyCommits:3, eventUrl:''},
+  {un:'dhh', dailyCommits:2, eventUrl:'' },
+  {un:'IAmEddieDean', dailyCommits:0, eventUrl:''},
+  {un:'EdsDover', dailyCommits:0, eventUrl:''},
+  {un:'chyld', dailyCommits:0, eventUrl:''},
+];
 function generateTiles() {
-  gitIds.forEach(function(users){
-    var url = 'https://api.github.com/users/'+users;
+  profiles.forEach(function(profile){
+    var url = 'https://api.github.com/users/'+profile.un;
     $.getJSON(url, function(response){
       var $newRow = $("#template").clone();
       $newRow.find(".image").attr("src", response.avatar_url);
       $newRow.find(".name").text(response.name);
       $newRow.removeClass("hidden");
       $('#cards-container').append($newRow);
+      console.log(response);
     });
   });
 }
