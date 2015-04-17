6b72c2ca89a7524950a8778618423922f3316bed
Edwin Dover <edsdover@gmail.com>
2015-04-17 15:16:18 -0700

    trying to dynamically generate profiles

--- client/index.js
+++ client/index.js
@@ -3,16 +3,47 @@
 $(document).ready(init);
 
 function init() {
-  generateTiles();
+  // generateTiles();
+  populateProfiles();
 }
+
+
+
+function populateProfiles(){
+  $.getJSON('https://api.github.com/orgs/coding-house-apr2015/members', function(loginResponse){
+    loginResponse.forEach(function(login){
+      (event.login).push(profiles1);
+    });
+  });
+}
+
 var currTime = moment.utc();
-var profiles = [
-  {un:'samerbuna', dailyCommits:0},
-  {un:'dhh', dailyCommits:0},
-  {un:'IAmEddieDean', dailyCommits:0},
-  {un:'EdsDover', dailyCommits:0},
-  {un:'chyld', dailyCommits:0}
-];
+var profiles1 = [{}];
+// var profiles = [
+//   {un:'dhh', dailyCommits:0},
+//   {un:'IAmEddieDean', dailyCommits:0},
+//   {un:'EdsDover', dailyCommits:0},
+//   {un:'chyld', dailyCommits:0},
+//   {un:'AmahAjavon', dailyCommits:0},
+//   {un:'andrewtdinh', dailyCommits:0},
+//   {un:'benhalverson', dailyCommits:0},
+//   {un:'cadenichols', dailyCommits:0},
+//   {un:'Cmarl', dailyCommits:0},
+//   {un:'greygatch', dailyCommits:0},
+//   {un:'kolohelios', dailyCommits:0},
+//   {un:'lifepupil', dailyCommits:0},
+//   {un:'ojangali', dailyCommits:0},
+//   {un:'rljarm', dailyCommits:0},
+//   {un:'SarahJessica', dailyCommits:0},
+//   {un:'sherylpeebee', dailyCommits:0},
+//   {un:'soyzamudio', dailyCommits:0},
+//   {un:'Spinaldash', dailyCommits:0},
+//   {un:'tania531', dailyCommits:0},
+//   {un:'TLawesomeness', dailyCommits:0},
+//   {un:'Coding-House', dailyCommits:0},
+//   {un:'edgecoder', dailyCommits:0},
+//   {un:'samerbuna', dailyCommits:0}
+// ];
 function generateTiles() {
   profiles.forEach(function(profile){
     var profileUrl = 'https://api.github.com/users/' + profile.un;
